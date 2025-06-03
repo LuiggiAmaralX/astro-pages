8154f445283d32a33f4dab04762958c91fdbbfbe
LuiggiAmaralX <luiggiamaral10@gmail.com>
2025-06-03 14:56:43 -0300

    add-login-css

--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { Header } from '../header/header';
 import { Footer } from '../footer/footer';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { Home } from '../home/home';
 
 @Component( {
   selector: 'app-login',
   standalone: true,
-  imports: [ Header, Footer ],
+  imports: [ Header, Footer, ReactiveFormsModule, RouterModule ],
   templateUrl: './login.html',
   styleUrl: './login.css'
 } )
